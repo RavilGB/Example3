4c192826c22b7a1e1116171de0d306de679bdfc3
Ravil <karimra@mail.ru>
2022-11-23 21:56:10 +0300

    nask2

---
+++ Task01/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Console.WriteLine("Ввидите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result;
+result = number * number;
+Console.WriteLine($"{number}^2 = {result}");
+*/
+//Task#2
+/*Console.WriteLine($"{a} и {b}");
+int a = Convert.ToInt32();
+int b = Convert.ToInt32();
+if (a * a == b);
+    {
+        Console.WriteLine("yes");
+    }
+else  (a * a = /2)
+*/
+/*Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+string result;
+    if(a * a == b){
+    result = "yes";
+}
+    else {
+result = "no";
+}
+Console.WriteLine(result);
+*/
+
+//Task3
+Console.WriteLine("Ввидите число от 1 до 7");
+int A = Convert.ToInt32(Console.ReadLine());
+string = result
+if(A == 1){
+Console.WriteLine("Понедельник");
+}
+else if
+
+    
